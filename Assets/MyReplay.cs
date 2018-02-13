9639e971a3474eacefdd059e2242738e1a2ec422
GIBO <aka-gibo@hotmail.com>
2018-02-13 22:08:57 +0300

    RU01 MyKeyFrame

---
+++ Assets/MyReplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyReplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MyKeyFrame myKeyFrame = new MyKeyFrame(Time.timeSinceLevelLoad, transform.position, transform.rotation);
+    
+	}
+}
+
+public struct MyKeyFrame {
+
+    public MyKeyFrame(float time, Vector3 posision, Quaternion rotasion)
+    {
+        Debug.Log("time: " + time + "....." + "pos: " + posision + "....." + "rot: " + rotasion + "/n");
+    }
+}
